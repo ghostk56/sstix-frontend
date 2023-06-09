11b65ab0bb016932e2a6745a4e4d462fe2f30d70
Sunny <ghostk56@gmail.com>
2023-06-09 18:10:08 +0800

    header logout options

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SHARED_ZORRO_MODULES } from '../../common/modules/shared-zorro.module';
-import { RouterModule } from '@angular/router';
+import { Route, Router, RouterModule } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 
 @Component({
   standalone: true,
   selector: 'app-admin',
-  imports: [CommonModule, RouterModule, SHARED_ZORRO_MODULES],
+  imports: [CommonModule, RouterModule, SHARED_ZORRO_MODULES,NzPopconfirmModule],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
 })
 export class MainComponent {
-  constructor(public loginService: LoginService) {}
+  constructor(public loginService: LoginService, private router: Router) {}
+
+  loggedOut() {
+    this.loginService.loggedOut();
+    this.router.navigate(['/login']);
+  }
 }
